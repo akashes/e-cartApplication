2d5019ef9513fa441b4d263f63ef81831b645e1e
akash es <akashes5753279@gmail.com>
2023-10-23 23:18:55 +0530

    created pages for the e cart application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import { useState } from 'react';
-import Display from './Display';
 
 function App() {
  
 
   return (
     <>
-    <div>
-      <h1 className='text-center mt-5'>counter Application</h1>
-      <Display/>
-    </div>
-    
+   
     </>
    
 
